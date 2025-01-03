e4190128a888b8ee328a2296474da2d38c0210d5
Gabriel FV <gabrielvasco906@gmail.com>
2025-01-03 18:33:50 -0300

    feat: update the create_view_model

--- lib/app/viewModels/create/create_view_model.dart
+++ lib/app/viewModels/create/create_view_model.dart
@@ -1,3 +1,13 @@
 import 'package:flutter/widgets.dart';
+import 'package:taski_to_do/app/repository/task_repository.dart';
 
-class CreateViewModel extends ChangeNotifier {}
+class CreateViewModel extends ChangeNotifier {
+  CreateViewModel(this._taskRepository);
+
+  final TaskRepository _taskRepository;
+
+  void addTask(String title, String description) {
+    _taskRepository.addTask(title, description);
+    notifyListeners();
+  }
+}
