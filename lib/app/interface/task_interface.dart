2bd3d0c461f2cf61b61ed927ccd3c95fc8f4acf7
Gabriel FV <gabrielvasco906@gmail.com>
2025-01-03 18:30:44 -0300

    feat: create an interface and update the repository

---
+++ lib/app/interface/task_interface.dart
@@ -0,0 +1,11 @@
+import 'package:taski_to_do/app/models/task_model.dart';
+
+abstract class ITaskRepository {
+  List<TaskModel> get tasks;
+  List<TaskModel> get completedTasks;
+  List<TaskModel> get pendingTasks;
+  void toggleTaskCompletion(int index);
+  void deleteTask(int index);
+  void deleteAll();
+  void addTask(String title, String description);
+}
--- lib/app/repository/task_repository.dart
+++ lib/app/repository/task_repository.dart
@@ -1,16 +1,21 @@
+import 'package:taski_to_do/app/interface/task_interface.dart';
 import 'package:taski_to_do/app/models/task_model.dart';
 
-class TaskRepository {
+class TaskRepository implements ITaskRepository {
   final List<TaskModel> _tasks = [];
 
+  @override
   List<TaskModel> get tasks => List.unmodifiable(_tasks);
 
+  @override
   List<TaskModel> get completedTasks =>
       _tasks.where((task) => task.isCompleted).toList();
 
+  @override
   List<TaskModel> get pendingTasks =>
       _tasks.where((task) => !task.isCompleted).toList();
 
+  @override
   void toggleTaskCompletion(int index) {
     if (index >= 0 && index < _tasks.length) {
       final task = _tasks[index];
@@ -19,6 +24,30 @@ class TaskRepository {
         description: task.description,
         isCompleted: !task.isCompleted,
       );
+    }
+  }
+
+  @override
+  void deleteTask(int index) {
+    if (index >= 0 && index < _tasks.length) {
+      _tasks.removeAt(index);
+    }
+  }
+
+  @override
+  void deleteAll() {
+    _tasks.clear();
+  }
+
+  @override
+  void addTask(String title, String description) {
+    if (title.isNotEmpty) {
+      final task = TaskModel(
+        title: title,
+        description: description,
+        isCompleted: false,
+      );
+      _tasks.add(task);
     }
   }
 }
