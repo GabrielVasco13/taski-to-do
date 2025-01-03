3e690f584e8472f405974e24377887fd2d35fbee
Gabriel FV <gabrielvasco906@gmail.com>
2025-01-03 18:30:55 -0300

    feat: update the done_view_model

--- lib/app/viewModels/done/done_view_model.dart
+++ lib/app/viewModels/done/done_view_model.dart
@@ -1,3 +1,21 @@
 import 'package:flutter/widgets.dart';
+import 'package:taski_to_do/app/models/task_model.dart';
+import 'package:taski_to_do/app/repository/task_repository.dart';
 
-class DoneViewModel extends ChangeNotifier {}
+class DoneViewModel extends ChangeNotifier {
+  DoneViewModel(this._taskRepository);
+
+  final TaskRepository _taskRepository;
+
+  List<TaskModel> get completedTasks => _taskRepository.completedTasks;
+
+  void deleteTask(int index) {
+    _taskRepository.deleteTask(index);
+    notifyListeners();
+  }
+
+  void deleteAll() {
+    _taskRepository.deleteAll();
+    notifyListeners();
+  }
+}
