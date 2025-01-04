6a23ab5393923e44b44df595622e4268f7aebaa2
Gabriel FV <gabrielvasco906@gmail.com>
2025-01-04 19:37:29 -0300

    feat: screen page created

--- lib/app/view/search/search_page.dart
+++ lib/app/view/search/search_page.dart
@@ -1,26 +1,52 @@
 import 'package:flutter/material.dart';
 import 'package:taski_to_do/app/view/widgets/app_bar_widget.dart';
 import 'package:taski_to_do/app/view/widgets/bottom_navigation_widget.dart';
+import 'package:taski_to_do/app/view/widgets/title_message_widget.dart';
 
-class SearchPage extends StatelessWidget {
+class SearchPage extends StatefulWidget {
   const SearchPage({super.key});
 
   @override
+  State<SearchPage> createState() => _SearchPageState();
+}
+
+class _SearchPageState extends State<SearchPage> {
+  @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
       backgroundColor: Colors.white,
-      appBar: AppBarWidget(),
-      bottomNavigationBar: BottomNavigationWidget(),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text(
-              'Welcome to Taski',
-              style: TextStyle(fontSize: 24),
+      appBar: const AppBarWidget(),
+      bottomNavigationBar: const BottomNavigationWidget(),
+      body: Column(
+        children: [
+          const SizedBox(height: 32),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 16.0),
+            child: TextFormField(
+              decoration: InputDecoration(
+                hintText: 'Search for your tasks!',
+                prefixIcon: const Icon(
+                  Icons.search,
+                  color: Colors.blue,
+                ),
+                filled: true,
+                fillColor: Colors.grey[200],
+                suffixIcon: IconButton(
+                  icon: const Icon(Icons.close),
+                  onPressed: () {},
+                ),
+                enabledBorder: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(16.0),
+                  borderSide: const BorderSide(color: Colors.blue, width: 2),
+                ),
+                focusedBorder: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(16.0),
+                  borderSide: const BorderSide(color: Colors.blue, width: 3),
+                ),
+              ),
             ),
-          ],
-        ),
+          )
+        ],
       ),
     );
   }
